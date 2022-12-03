0a48de86a367aad1aa2308e4d9a2d5e03cac999d
MariaClaudiaWandersee <mariaaclaudiaa2401@gmail.com>
2022-12-02 21:10:02 -0300

    salvando no banco

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import './App.css';
+import Axios from "axios";
 
 function App() {
   const [values,SetValues] = useState();
@@ -13,8 +14,14 @@ function App() {
   };
 
   const handleClickButton = () =>{
-    console.log(values);
-  }
+    Axios.post("http://localhost:3001/register", {
+      name: values.name,
+      cost: values.cost,
+      category: values.category,
+    }).then((response)=>{
+      console.log(response)
+    });
+  };
 
   return (
     <div className="app--container">
